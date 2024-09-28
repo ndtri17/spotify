b169c1038fadb9e0369e8a38492f069b5583e895
Treee <tri.nghiem@epitech.eu>
2024-09-28 22:25:34 +0200

    end

--- spotify_backend/controller/ArtistController.js
+++ spotify_backend/controller/ArtistController.js
@@ -1,48 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const database = require('../config/db');
+const database = require("../config/db");
 
+router.get("/popular_artists", (req, res) => {
+  const query = "SELECT * FROM artists ORDER BY RAND() LIMIT 8";
 
-router.get('/popular_artists', (req, res) => {
-
-    const query = 'SELECT * FROM artists ORDER BY RAND() LIMIT 8';
-    
-    database.query(query, (error, results) => {
-        if (error) {
-            console.error('Error retrieving popular artists:', error);   
-            res.status(500).send('Error retrieving popular artists');
-        } else {
-            res.status(200).json(results); 
-        }
-    });
+  database.query(query, (error, results) => {
+    if (error) {
+      console.error("Error retrieving popular artists:", error);
+      res.status(500).send("Error retrieving popular artists");
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
-router.get('/artistes', (req, res) => {
-    
-    const query = 'SELECT * FROM artists';
-    
-    database.query(query, (error, results) => {
-        if (error) {
-            console.error('Error retrieving artistes:', error);   
-            res.status(500).send('Error retrieving artistes');
-        } else {
-            res.status(200).json(results); 
-        }
-    }); 
+router.get("/artistes", (req, res) => {
+  const query = "SELECT * FROM artists";
+
+  database.query(query, (error, results) => {
+    if (error) {
+      console.error("Error retrieving artistes:", error);
+      res.status(500).send("Error retrieving artistes");
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
-router.get('/artistes/:artisteId', (req, res) => {
-
-    const query = 'SELECT artists.name , artists.photo , albums.name , albums.cover FROM artists JOIN albums ON artists.id = albums.artist_id WHERE artists.id = ?';
-    
-    database.query(query, [req.params.artisteId], (error, results) => {
-        if (error) {
-            console.error('Error retrieving artist:', error);   
-            res.status(500).send('Error retrieving artist');
-        } else {
-            res.status(200).json(results); 
-        }
-    });
+router.get("/artistes/:artisteId", (req, res) => {
+  const query = `
+    SELECT artists.*, albums.id AS album_id, albums.name AS album_name, albums.cover AS album_cover
+    FROM artists 
+    LEFT JOIN albums ON artists.id = albums.artist_id 
+    WHERE artists.id = ?
+`;
+  database.query(query, [req.params.artisteId], (error, results) => {
+    if (error) {
+      console.error("Error retrieving artist:", error);
+      res.status(500).send("Error retrieving artist");
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
-module.exports = router
+module.exports = router;
--- spotify_frontend/src/pages /ArtistePage.jsx
+++ spotify_frontend/src/pages /ArtistePage.jsx
@@ -1,17 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import api from "../api/api";
 import "../styles/ArtistePage.css";
 
 const ArtistePage = () => {
   const { artistId } = useParams();
-  const [artist, setArtist] = useState([]);
+  const [artist, setArtist] = useState(null);
+  const [albums, setAlbums] = useState([]);
+
+  const navigate = useNavigate();
 
   const fetchArtist = async () => {
     try {
       const response = await api.get(`artistes/${artistId}`);
-      if (response.data) {
-        setArtist(response.data[0]);
+      if (response.data.length > 0) {
+        const artistData = {
+          id: response.data[0].id,
+          name: response.data[0].name,
+          description: response.data[0].description,
+          bio: response.data[0].bio,
+          photo: response.data[0].photo,
+        };
+        setArtist(artistData);
+
+        const albumsData = response.data.map(album => ({
+          id: album.album_id,
+          name: album.album_name,
+          cover: album.album_cover,
+          releaseDate: album.release_date,
+        })).filter(album => album.id); 
+
+        setAlbums(albumsData);
       }
     } catch (error) {
       console.error("Error fetching artist:", error);
@@ -22,12 +41,40 @@ const ArtistePage = () => {
     fetchArtist();
   }, []);
 
-
-
   return (
-    <div>
-        <h1>{artist.artist}</h1>
-        
+    <div className="artistePage">
+      {artist && (
+        <div className="artistePage__header">
+          <img
+            className="artistePage__headerImage"
+            src={artist.photo}
+            alt={artist.name}
+          />
+          <div className="artistePage__info">
+            <h1 className="artistePage__name">{artist.name}</h1>
+            <p className="artistePage__bio">{artist.bio}</p>
+          </div>
+        </div>
+      )}
+
+      <div className="artistePage__content">
+        <h2>Albums</h2>
+        <div className="artistePage__albums">
+          {albums.length > 0 ? (
+            albums.map((album) => (
+              <div key={album.id} className="artistePage__albumCard" onClick={() => navigate(`/album/${album.id}`)}>
+                <img
+                  className="artistePage__albumImage"
+                  src={album.cover}
+                  alt={album.name}
+                />
+              </div>
+            ))
+          ) : (
+            <p>No albums available.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
